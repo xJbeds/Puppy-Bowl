60ac461c59001c2979c86c73e9b20bb974d37044
Jake Bedford <robertjakebedford@gmail.com>
2025-06-04 07:49:57 -0600

    1st initial push, with linking API and having the user see the ul

--- index.js
+++ index.js
@@ -1,4 +1,39 @@
+// console.log(`TESTING`);
+
 // === Constants ===
 const BASE = "https://fsa-puppy-bowl.herokuapp.com/api";
-const COHORT = "/"; // Make sure to change this!
-const API = BASE + COHORT;
+const COHORT = "2505-ftb-web-ft/players"; // Make sure to change this!
+const API = `${BASE}/${COHORT}`;
+
+const state = {
+  puppies: [],
+
+};
+
+const app = document.getElementById(`app`);
+
+const puppyList = document.createElement(`ul`);
+puppyList.innerHTML = `puppy-list`;
+app.appendChild(puppyList);
+
+const getPuppyPlayers = async () => {
+  const response = await fetch(API);
+  const responseJsonObject = await response.json();
+  state.puppies = responseJsonObject.data.players;
+
+  renderPuppies();
+};
+
+const renderPuppies = () => {
+  puppyList.innerHTML = ``;
+
+  state.puppies.forEach((puppy) => {
+    const puppyUl = document.createElement(`ul`);
+    puppyUl.textContent = puppy.name;
+    puppyList.appendChild(puppyUl);
+  });
+};
+
+getPuppyPlayers();
+
+
